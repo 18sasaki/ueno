916b99a94361224690b9373225b4450409fba218
18sasaki <18sasaki@gmail.com>
2014-03-25 20:25:22 +0900

    add basic views and models

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+#coding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'active_record'
@@ -5,6 +7,43 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/myapp.db')
 
 get '/' do
-  'Heroku/Sinatra/Hello world!'
+  @authors = Author.all
+  erb :index
 end
 
+get '/show' do
+  @author = Author.find(params[:id]) rescue Author.new
+  erb :show
+end
+
+post '/new' do
+  author = Author.new
+  author.id = params[:id]
+  author.name = params[:name]
+  author.name_kana = params[:name_kana]
+  author.initial = params[:initial]
+  author.sex = params[:sex]
+  author.save
+  redirect '/'
+end
+
+delete '/del' do
+  author = Author.find(params[:id])
+  author.destroy
+  redirect '/'
+end
+
+
+class Author < ActiveRecord::Base
+end
+
+
+helpers do
+  def sex_hash
+    { '0' => '未設定', '1' => '男', '2' => '女', '3' => '非公開' }
+  end
+
+  def sex_translate(sex_id)
+    sex_hash[sex_id.to_s]
+  end
+end
