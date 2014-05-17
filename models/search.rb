deb5b44f2f2da87e7111c854ae58092554bb91e3
18sasaki <18sasaki@gmail.com>
2014-05-17 12:55:57 +0900

    検索結果表示画面追加

--- models/search.rb
+++ models/search.rb
@@ -4,10 +4,11 @@ class Search < ActiveRecord::Base
   extend RaiseCatch
 
   def self.search_by_isbn(isbn_str)
-    item = Amazon::Ecs.item_lookup(isbn_str, {IdType: 'ISBN', SearchIndex: 'Books'}).first_item
+    item = Amazon::Ecs.item_lookup(isbn_str, {IdType: 'ISBN', SearchIndex: 'Books', ResponseGroup: 'ItemAttributes'}).first_item
 
     {
-      isbn:      isbn_str,
+      isbn_str:  isbn_str,
+      isbn:      item.get('ItemAttributes/ISBN'),
       title:     item.get('ItemAttributes/Title'),
       author:    item.get('ItemAttributes/Author'),
       publisher: item.get('ItemAttributes/Manufacturer')
